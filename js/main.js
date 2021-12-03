67241706be45b3863902cda9f5ea9d1f80f7264f
Neo <hitomi601423@gmail.com>
2021-12-03 12:18:58 +0900

    beteween-commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+
+
+
 const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
 });
